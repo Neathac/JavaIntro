d9f2a990efc270c555da2e68f9394b0dec291183
Martin Gráf <Neath.graf@gmail.com.com>
2022-10-07 17:12:44 +0200

    Working solution

--- HW_01/src/CodEx.java
+++ HW_01/src/CodEx.java
@@ -1,11 +1,19 @@
 public class CodEx {
     public static void main(String[] args) throws Exception {
-        try {
-            for (int i = 1; i < 10; ++i) {
-                System.out.println(" " + i + " * " + args[0] + " = " + (i*Integer.parseInt(args[0])));
-            }
-            System.out.println(10 + " * " + args[0] + " = " + (10*Integer.parseInt(args[0])));
-        } catch (Exception e) {}
-        
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(getIndentedResult(i, 2) + " * " + args[0] +
+            " = " + getIndentedResult(i*Integer.parseInt(args[0]), 1+args[0].length()));
+        }
+    }
+
+    public static String getIndentedResult(int toIndent, int maxLength) {
+        String result = "";
+        double order = maxLength - Integer.toString(toIndent).length();
+        while (order > 0) {
+            order -= 1;
+            result += " ";
+        }
+        result += Integer.toString(toIndent);
+        return result;
     }
 }
